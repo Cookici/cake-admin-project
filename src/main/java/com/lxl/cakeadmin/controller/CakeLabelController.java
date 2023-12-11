495715bd0ced6b30c6e8bfe7a700cc7bddface43
Xia Rui <2257045699@qq.com>
2023-12-11 11:27:09 +0800

    夏睿

--- src/main/java/com/lxl/cakeadmin/controller/CakeLabelController.java
+++ src/main/java/com/lxl/cakeadmin/controller/CakeLabelController.java
@@ -8,10 +8,11 @@ import com.lxl.cakeadmin.result.Result;
 import com.lxl.cakeadmin.service.CakeLabelService;
 import com.lxl.cakeadmin.service.CakePermitService;
 import com.lxl.cakeadmin.vo.PageVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @ProjectName: cake-admin
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  * @Date: 2023/12/7 12:31
  */
+@Slf4j
 @RestController
 @RequestMapping("/label")
 public class CakeLabelController {
@@ -32,6 +34,42 @@ public class CakeLabelController {
     public Result<IPage<CakeLabel>> getPermitList(PageVo pageVo) {
         Page<CakeLabel> page = cakeLabelService.page(new Page<>(pageVo.getCurrent(), pageVo.getSize()));
         return Result.ok(page);
+    }
+
+    @PostMapping("/add")
+    public Result<String> addLabel(@RequestBody CakeLabel cakeLabel){
+        boolean save = cakeLabelService.save(cakeLabel);
+        if(save){
+            return Result.ok("添加成功");
+        }
+        return Result.fail("添加失败");
+    }
+
+    @PutMapping("/edit")
+    public Result<String> updateLabel(@RequestBody CakeLabel cakeLabel){
+        boolean update = cakeLabelService.updateById(cakeLabel);
+        if(update){
+            return Result.ok("更新成功");
+        }
+        return Result.fail("更新失败");
+    }
+
+    @DeleteMapping("/delete")
+    public Result<String> removeLabel(@RequestParam Long id){
+        boolean remove = cakeLabelService.removeById(id);
+        if(remove){
+            return Result.ok("删除成功");
+        }
+        return Result.fail("删除失败");
+    }
+
+    @DeleteMapping("/deleteList")
+    public Result<String> removeLabelByIds(@RequestParam List<Long> ids){
+        boolean remove = cakeLabelService.removeByIds(ids);
+        if (remove){
+            return Result.ok("删除成功");
+        }
+        return  Result.fail("删除失败");
     }
 
 }
--- src/main/java/com/lxl/cakeadmin/controller/CakePermitController.java
+++ src/main/java/com/lxl/cakeadmin/controller/CakePermitController.java
@@ -8,9 +8,9 @@ import com.lxl.cakeadmin.result.Result;
 import com.lxl.cakeadmin.service.CakePermitService;
 import com.lxl.cakeadmin.vo.PageVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @ProjectName: cake-admin
@@ -32,5 +32,42 @@ public class CakePermitController {
         Page<CakePermit> page = cakePermitService.page(new Page<>(pageVo.getCurrent(), pageVo.getSize()));
         return Result.ok(page);
     }
+
+    @PostMapping("/add")
+    public Result<String> addPermit(@RequestBody CakePermit cakePermit){
+        boolean save = cakePermitService.save(cakePermit);
+        if(save){
+            return Result.ok("添加成功");
+        }
+        return Result.fail("添加失败");
+    }
+
+    @PutMapping("/edit")
+    public Result<String> updatePermit(@RequestBody CakePermit cakePermit){
+        boolean update = cakePermitService.updateById(cakePermit);
+        if(update){
+            return Result.ok("更新成功");
+        }
+        return Result.fail("更新失败");
+    }
+
+    @DeleteMapping("/delete")
+    public Result<String> removePermit(@RequestParam Long id){
+        boolean remove = cakePermitService.removeById(id);
+        if(remove){
+            return Result.ok("删除成功");
+        }
+        return Result.fail("删除失败");
+    }
+
+    @DeleteMapping("/deleteList")
+    public Result<String> removePermitByIds(@RequestParam List<Long> ids){
+        boolean remove = cakePermitService.removeByIds(ids);
+        if(remove){
+            return Result.ok("删除成功");
+        }
+        return Result.fail("删除失败");
+    }
+
 
 }
